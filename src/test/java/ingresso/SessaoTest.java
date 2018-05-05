794304355d95e621159e83bd91fe228f23c07086
Fabio de Almeida <fabyiyo1@gmail.com>
2018-05-05 14:35:21 -0300

    aplicando Descontos

---
+++ src/test/java/ingresso/SessaoTest.java
@@ -0,0 +1,30 @@
+package ingresso;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import junit.framework.Assert;
+
+public class SessaoTest {
+	
+	@Test
+	public void oPrecoDaSessaoDeveSerIgualASomaDaSalaMaisOPrecoDoFilme(){
+		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("22.5"));
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCF-FI", new BigDecimal("12.0"));
+		
+		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
+		
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		
+		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
+		
+		
+	}
+
+}
